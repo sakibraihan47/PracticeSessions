84e0a4a1d3e5ecb1f3f1f0f40b8fd532eadfc681
sakibraihan47 <sakibraihan47@gmail.com>
2022-10-20 18:51:29 +0600

    Pushing database connection file

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.on("connected", (error) => {
+  console.log("Database is Connected");
+});
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server Started at ${3000}");
+});
